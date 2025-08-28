ad3190c3d83694bcb264612ec80e34558568342e
TalhaZobayer <talhazobayer2005@gmail.com>
2025-08-28 11:32:37 +0600

    Add files via upload

---
+++ Simple Calculator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   long long int X,Y,summation,multiplication,subtraction;
+    cin>>X>>Y;
+
+    summation= X+Y;
+    multiplication= X*Y;
+    subtraction =X-Y;
+
+   cout<<X<<" + "<<Y<<" = "<<summation<<endl;
+   cout<<X<<" * "<<Y<<" = "<<multiplication<<endl;
+   cout<<X<<" - "<<Y<<" = "<<subtraction<<endl;
+
+}
